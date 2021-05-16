cc7f8720fdff8caf14e9b5ceb9bf783907e32b16
Restyled.io <commits@restyled.io>
2021-05-16 16:45:36 +0000

    Restyled by autopep8

--- server.py
+++ server.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def render_static():
-    return render_template('index.html') #add your file here
+    return render_template('index.html')  # add your file here
+
 
 if __name__ == '__main__':
     app.run()
